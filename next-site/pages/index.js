c34eb3fe799ffe16425a29ee7fb78282f073ba3b
astroboy28 <eicalla2011@gmail.com>
2021-09-28 19:16:23 +0900

    Creating a Layout Component

--- next-site/pages/index.js
+++ next-site/pages/index.js
@@ -1,16 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>HomePage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nihil eius, aperiam qui dolor blanditiis ea pariatur. Id, odio voluptatibus.</p>
-      <Footer />
     </div>
   )
 }
+
+
