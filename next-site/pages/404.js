1784fb8d085d0f9f06b9ab7c57fbdf6b73c2d1b2
astroboy28 <eicalla2011@gmail.com>
2021-09-29 09:06:26 +0900

    Redirecting Users

--- next-site/pages/404.js
+++ next-site/pages/404.js
@@ -1,6 +1,17 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            // router.go(-1) this method can be used to redirect or go back to the last visited page or (1) one page forward
+            router.push('/'); // this method redirects a user to the homepage in 3 seconds.
+        }, 3000)
+    }, [])
+
     return ( 
         <div className="notFound">
             <h1>Oooopppss..</h1>
