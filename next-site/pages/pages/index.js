70c88def28d19ff02ad656996952a03717289866
astroboy28 <eicalla2011@gmail.com>
2021-09-29 10:12:07 +0900

    Fetching Data (getStaticProps)

--- next-site/pages/pages/index.js
+++ next-site/pages/pages/index.js
@@ -1,11 +1,28 @@
 import styles from '../../styles/myStyles.module.css'
 
-const PagesIndex = () => {
+export const getStaticProps = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: { fetchUsers: data }
+    }
+}
+
+const FetchData = ({fetchUsers}) => {
     return ( 
         <div className={styles.container}>
-            <h1>Pages Index page</h1>
+            <h1>Fetching Data</h1>
+            {fetchUsers.map(userWatever => (
+                <div key={ userWatever.id }> 
+                    <a className={styles.single}>
+                        <h3>{ userWatever.name }</h3>
+                    </a>
+                </div>
+            ))}
         </div>
      );
 }
  
-export default PagesIndex;
+export default FetchData;
